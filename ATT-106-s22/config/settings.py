0c538b743f58535a2db86175f827582be9b8b057
Ilhomjon <githubg348@gmail.com>
2023-02-09 14:51:30 +0500

    uzgarish

--- ATT-106-s22/config/settings.py
+++ ATT-106-s22/config/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config, Csv
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ee2skup&x&nfth3$quw-6=e6)s37ielrxvzwrw^k88zlbupm7c'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', default=False, cast=bool)
+config('ALLOWED_HOSTS', cast=Csv())
 
 
 # Application definition
